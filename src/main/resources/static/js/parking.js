734d4118a947dd5401af1dc99426a35586ef9e8e
ALESSANDRO UMBERTO DANIELE SALTARIN <alessandroumbertods@ufps.edu.co>
2023-08-06 23:15:48 -0500

    Implement Offline Support with LocalStorage
    This commit introduces offline support using LocalStorage. When the application is offline, car data is stored in the LocalStorage, and when the internet connection is restored, the saved data is sent to the server. This feature ensures that users can register cars even without an internet connection and syncs the data when the connection becomes available again.

--- src/main/resources/static/js/parking.js
+++ src/main/resources/static/js/parking.js
@@ -28,7 +28,7 @@ function closeModal() {
 
 async function loadCars() {
     const id = localStorage.getItem('id');
-    const request = await fetch('/api/cars/' + id , {
+    const request = await fetch('/api/cars/' + id, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -47,76 +47,91 @@ async function loadCars() {
         }
         car.pay = (car.pay == null) ? 0 : car.pay;
         car.credit = (car.credit == null) ? 0 : car.credit;
-        let retireButton = '<a href="#" onClick="retireCar(\'' + car.licensePlate + '\')" class="btn btn-danger btclassNameNamecle btn-sm"><i class="fas fa-trash" alt="Retirar Carro"></i> </a>';
+        let retireButton = '<a href="#" onClick="retireCar({\'licensePlate\': \'' + car.licensePlate + '\', \'model\': \'' + car.model + '\', \'brand\': \'' + car.brand + '\', \'property\': \'' + car.property + '\', \'origin\': \'' + car.origin + '\', \'dateTime\': \'' + car.dateTime + '\', \'room\': \'' + car.room + '\', \'credit\': \'' + car.credit + '\', \'pay\': \'' + car.pay + '\'})" class="btn btn-danger btclassNameNamecle btn-sm"><i class="fas fa-trash" alt="Retirar Carro"></i> </a>';
         let updateButton = '<a href="#" onclick="openModal({\'licensePlate\': \'' + car.licensePlate + '\', \'model\': \'' + car.model + '\', \'brand\': \'' + car.brand + '\', \'property\': \'' + car.property + '\', \'origin\': \'' + car.origin + '\', \'dateTime\': \'' + car.dateTime + '\', \'room\': \'' + car.room + '\', \'credit\': \'' + car.credit + '\', \'pay\': \'' + car.pay + '\'})" class="btn btn-info btn-sm"><i class="fas fa-info" alt="Actualizar carro"></i></a>';
         let buttonsContainer = '<div class="buttons-container">' + retireButton + updateButton + '</div>';
         let carInformation = '<tr><td>' + car.model + '</td><td>' + car.brand + '</td><td>' + car.licensePlate + '</td><td>' + car.property + '</td><td>' + car.origin + '</td><td>' + car.dateTime + '</td><td>' + car.pay.toLocaleString('es-ES', option) + '</td><td>' + car.credit.toLocaleString('es-ES', option) + '</td><td>' + car.room + '</td><td>' + buttonsContainer + '</td></tr>'
         carsData += carInformation;
 
     }
-
     document.querySelector('#parking tbody').outerHTML = carsData;
 }
 
-async function retireCar(licensePlate) {
-    const carInfo = await getCar(licensePlate);
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: 'btn btn-success',
-            cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-    })
+async function retireCar(carInfo) {
+    //const carInfo = await getCar(licensePlate);
+    if (navigator.onLine) {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
 
-    swalWithBootstrapButtons.fire({
-        title: '¿Estás seguro que quieres retirar el carro?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Si, retirar',
-        cancelButtonText: 'No, cancelar!',
-        reverseButtons: true
-    }).then(async (result) => {
-        if (result.isConfirmed) {
-            carInfo.pay = (carInfo.pay == null) ? 0 : carInfo.pay;
-            carInfo.credit = (carInfo.credit == null) ? 0 : carInfo.credit;
+        swalWithBootstrapButtons.fire({
+            title: '¿Estás seguro que quieres retirar el carro?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Si, retirar',
+            cancelButtonText: 'No, cancelar!',
+            reverseButtons: true
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                carInfo.pay = (carInfo.pay == null) ? 0 : carInfo.pay;
+                carInfo.credit = (carInfo.credit == null) ? 0 : carInfo.credit;
+                carInfo.active = false;
 
-            let option = {useGrouping: true};
+                const message = await carRetireData(carInfo)
+                /*
+                carInfo.pay = (carInfo.pay == null) ? 0 : carInfo.pay;
+                carInfo.credit = (carInfo.credit == null) ? 0 : carInfo.credit;
 
-            let dateCar = new Date(carInfo.dateTime)
-            dateCar.setHours(24)
-            let actualDate = new Date()
+                let option = {useGrouping: true};
 
-            const difference = actualDate - dateCar;
+                let dateCar = new Date(carInfo.dateTime)
+                dateCar.setHours(24)
+                let actualDate = new Date()
 
-            let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-            days = (days === 0) ? 1 : days;
+                const difference = actualDate - dateCar;
 
-            const price = ((carInfo.pay * days) - carInfo.credit).toLocaleString('es-ES', option);
-            const stringDay = (days === 1) ? ' dia' : ' dias';
+                let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+                days = (days === 0) ? 1 : days;
 
-            const credit = (carInfo.credit === 0) ? '' : ' abonó ' + carInfo.credit.toLocaleString('es-ES', option);
+                const price = ((carInfo.pay * days) - carInfo.credit).toLocaleString('es-ES', option);
+                const stringDay = (days === 1) ? ' dia' : ' dias';
 
+                const credit = (carInfo.credit === 0) ? '' : ' abonó ' + carInfo.credit.toLocaleString('es-ES', option);
 
-            await swalWithBootstrapButtons.fire(
-                'Retirado',
-                'El carro ' + carInfo.brand + ' ' + carInfo.model + ' estuvo  ' + days + stringDay + credit + '  y debe ' + price + '$'
-            )
-            carInfo.active = false;
-            await updateCar(carInfo);
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-            swalWithBootstrapButtons.fire(
-                'Cancelado',
-            )
-        }
+                carInfo.active = false;
 
-    })
+                 */
+
+                await swalWithBootstrapButtons.fire({
+                        icon: 'success',
+                        title: 'Retirado',
+                        text: message
+                    }
+                )
+
+                await updateCar(carInfo);
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                swalWithBootstrapButtons.fire({
+                        icon: 'error',
+                        title: 'Cancelado'
+                    }
+                )
+            }
+        })
+    } else {
+        confirmWithdrawCar(carInfo)
+    }
 }
 
 
 async function getCar(licensePlate) {
     const id = localStorage.getItem('id');
     try {
-        const response = await fetch('/api/car/' + id + '/'+encodeURIComponent(licensePlate), {
+        const response = await fetch('/api/car/' + id + '/' + encodeURIComponent(licensePlate), {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -130,17 +145,72 @@ async function getCar(licensePlate) {
 }
 
 async function updateCar(carInfo) {
-    try {
-        await fetch('/api/car/' + carInfo.licensePlate, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(carInfo)
-        });
-        location.reload();
-    } catch (e) {
-        return null;
+    if (navigator.onLine) {
+        try {
+            await fetch('/api/car/' + carInfo.licensePlate, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(carInfo)
+            });
+            location.reload();
+        } catch (e) {
+            return null;
+        }
+    } else {
+        const deleteCars = JSON.parse(localStorage.getItem('deleteCars')) || [];
+        deleteCars.push(carInfo);
+        localStorage.setItem('deleteCars', JSON.stringify(deleteCars));
+        alert('El carro ha sido retirado localmente. Se enviará los datos cuando haya conexión a internet');
     }
 }
+
+async function confirmWithdrawCar(carInfo) {
+    const result = confirm("¿Seguro que quieres retirar el carro?");
+    if (result === true) {
+        const message = await carRetireData(carInfo);
+        await updateCar(carInfo);
+        alert(message)
+    }
+}
+
+async function carRetireData(carInfo) {
+    carInfo.pay = (carInfo.pay == null) ? 0 : carInfo.pay;
+    carInfo.credit = (carInfo.credit == null) ? 0 : carInfo.credit;
+
+    let option = {useGrouping: true};
+
+    let dateCar = new Date(carInfo.dateTime)
+    dateCar.setHours(24)
+    let actualDate = new Date()
+
+    const difference = actualDate - dateCar;
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    days = (days === 0) ? 1 : days;
+
+    const price = ((carInfo.pay * days) - carInfo.credit).toLocaleString('es-ES', option);
+    const stringDay = (days === 1) ? ' dia' : ' dias';
+
+    const credit = (carInfo.credit === 0) ? '' : ' abonó ' + carInfo.credit.toLocaleString('es-ES', option);
+
+    carInfo.active = false;
+    return 'El carro ' + carInfo.brand + ' ' + carInfo.model + ' estuvo  ' + days + stringDay + credit + '  y debe ' + price + '$';
+}
+
+function checkOnlineStatus() {
+    if (navigator.onLine) {
+        const deleteCars = JSON.parse(localStorage.getItem('deleteCars'));
+        if (deleteCars && deleteCars.length > 0) {
+            deleteCars.forEach(async (carInfo) => {
+                await updateCar(carInfo);
+            });
+            localStorage.removeItem('deleteCars');
+            alert('Los carros retirados localmente han sido enviados al servidor.');
+        }
+    }
+}
+
+window.addEventListener('online', checkOnlineStatus);
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -17,26 +17,42 @@ async function registerCar() {
     dates.active = true;
     dates.room = document.getElementById('txtRoom').value;
 
+    await saveCar(dates);
+}
+async function saveCar(dates){
     const id = localStorage.getItem('id');
+    if (navigator.onLine) {
+        const request = await fetch('/api/car/' + id, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dates)
+        })
 
-    const request = await fetch('/api/car/' + id, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dates)
-    })
-
-    await Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'El carro se ha registrado correctamente',
-        timer: 1500,
-        showConfirmButton: false,
-        closeOnClickOutside: false
-    })
-    location.reload()
+        await Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'El carro se ha registrado correctamente',
+            timer: 1500,
+            showConfirmButton: false,
+            closeOnClickOutside: false
+        })
+        location.reload()
+    }else{
+        const savedCars = JSON.parse(localStorage.getItem('savedCars')) || [];
+        savedCars.push(dates);
+        localStorage.setItem('savedCars', JSON.stringify(savedCars));
+        alert('El carro ha sido guardado localmente. Se enviará los datos cuando haya conexión a internet');
+        let option = {useGrouping: true};
+        let retireButton = '<a href="#" onClick="retireCar({\'licensePlate\': \'' + dates.licensePlate + '\', \'model\': \'' + dates.model + '\', \'brand\': \'' + dates.brand + '\', \'property\': \'' + dates.property + '\', \'origin\': \'' + dates.origin + '\', \'dateTime\': \'' + dates.dateTime + '\', \'room\': \'' + dates.room + '\', \'credit\': \'' + dates.credit + '\', \'pay\': \'' + dates.pay + '\'})" class="btn btn-danger btclassNameNamecle btn-sm"><i class="fas fa-trash" alt="Retirar Carro"></i> </a>';
+        let updateButton = '<a href="#" onclick="openModal({\'licensePlate\': \'' + dates.licensePlate + '\', \'model\': \'' + dates.model + '\', \'brand\': \'' + dates.brand + '\', \'property\': \'' + dates.property + '\', \'origin\': \'' + dates.origin + '\', \'dateTime\': \'' + dates.dateTime + '\', \'room\': \'' + dates.room + '\', \'credit\': \'' + dates.credit + '\', \'pay\': \'' + dates.pay + '\'})" class="btn btn-info btn-sm"><i class="fas fa-info" alt="Actualizar carro"></i></a>';
+        let buttonsContainer = '<div class="buttons-container">' + retireButton + updateButton + '</div>';
+        const carInformation = `<tr><td>${dates.model}</td><td>${dates.brand}</td><td>${dates.licensePlate}</td><td>${dates.property}</td><td>${dates.origin}</td><td>${dates.dateTime}</td><td>${dates.pay.toLocaleString('es-ES', option)}</td><td>${dates.credit.toLocaleString('es-ES', option)}</td><td>${dates.room}</td><td>${buttonsContainer}</td></tr>`;
+        const tableBody = document.querySelector('#parking tbody');
+        tableBody.insertAdjacentHTML('beforeend', carInformation);
+    }
 }
 
 function cleanData(data) {
@@ -53,3 +69,19 @@ function formatFirstLetterToUpperCase(value) {
         return formattedValue;
     }
 }
+
+function checkOnlineStatus() {
+    if (navigator.onLine) {
+        const savedCars = JSON.parse(localStorage.getItem('savedCars'));
+        if (savedCars && savedCars.length > 0) {
+            savedCars.forEach(async (dates) => {
+                await saveCar(dates);
+            });
+            localStorage.removeItem('savedCars');
+            alert('Los carros guardados localmente han sido enviados al servidor.');
+        }
+    }
+}
+
+// Escuchar el evento online/offline para verificar cuando se restablece la conexión
+window.addEventListener('online', checkOnlineStatus);
